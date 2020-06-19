ad32622fcd8e9486b07809ec59cbe1872473c805
Masaaki Goshima <goccy54@gmail.com>
2020-06-19 15:28:56 +0900

    Add test case for reporter package

--- reporter/testing_test.go
+++ reporter/testing_test.go
@@ -5,3 +5,12 @@ import "testing"
 func TestFromT(t *testing.T) {
 	var _ Reporter = FromT(t)
 }
+
+func Test_TestingLog(t *testing.T) {
+	rptr := FromT(t)
+	rptr.Log("log test")
+	rptr.Run("subtest", func(rptr Reporter) {
+		rptr.Logf("%s", "log test")
+		rptr.Skip()
+	})
+}

9d7ab73640fb85dfef4e513a94c962855c2b41be
zoncoen <zoncoen@gmail.com>
2023-01-24 03:38:05 +0900

    feat(http): add text/html unmarshaler

--- protocol/http/unmarshaler/html.go
+++ protocol/http/unmarshaler/html.go
@@ -7,20 +7,20 @@ import (
 )
 
 func init() {
-	if err := Register(&textUnmarshaler{}); err != nil {
+	if err := Register(&htmlUnmarshaler{}); err != nil {
 		panic(err)
 	}
 }
 
-type textUnmarshaler struct{}
+type htmlUnmarshaler struct{}
 
 // MediaType implements ResponseUnmarshaler interface.
-func (um *textUnmarshaler) MediaType() string {
-	return "text/plain"
+func (um *htmlUnmarshaler) MediaType() string {
+	return "text/html"
 }
 
 // Unmarshal implements ResponseUnmarshaler interface.
-func (um *textUnmarshaler) Unmarshal(data []byte, v interface{}) error {
+func (um *htmlUnmarshaler) Unmarshal(data []byte, v interface{}) error {
 	rv := reflect.ValueOf(v)
 	if rv.Kind() != reflect.Ptr {
 		return errors.New("v must be a pointer")
